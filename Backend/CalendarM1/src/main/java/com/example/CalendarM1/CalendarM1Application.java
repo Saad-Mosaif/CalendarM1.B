989862198f0d52bead6a466e89f5032ddc86fd32
Saad Mosaif <saadmosaif01@gmail.com>
2024-10-16 22:42:31 +0100

    first commit

---
+++ Backend/CalendarM1/src/main/java/com/example/CalendarM1/CalendarM1Application.java
@@ -0,0 +1,13 @@
+package com.example.CalendarM1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarM1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarM1Application.class, args);
+	}
+
+}
